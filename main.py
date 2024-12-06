a8647b8208a8397bcd6cb9698e23fd076576b9d1
Miguel Dominic Payumo <150775008+6Domx@users.noreply.github.com>
2024-12-06 16:35:46 +0800

    Update main.py for navigation.

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
-
+@app.route('/')
 @app.route("/home")
-@app.route("/")
 def home():
     return render_template("home.html")
 
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
+@app.route("/works")
+def works():
+    return render_template("works.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
+
+
