6a9fb966d44d44fc7410e51e9b6e5336818f4f06
Miguel Dominic Payumo <150775008+6Domx@users.noreply.github.com>
2024-12-08 16:02:54 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from Linked_list import LinkedList
 
 app = Flask(__name__)
-@app.route('/')
+
+linked_list = LinkedList()  
+
+@app.route("/")
 @app.route("/home")
 def home():
     return render_template("home.html")
@@ -14,7 +18,31 @@ def profile():
 def works():
     return render_template("works.html")
 
+@app.route("/contacts")
+def contacts():
+    return render_template("contacts.html")
+
+@app.route('/linked_list', methods=['GET'])  
+def show_linked_list():  
+    return render_template('linked_list.html', linked_list=linked_list.print_list())
+
+@app.route('/update', methods=['POST'])
+def update():
+    operation = request.form['operation']
+    data = request.form.get('data', None)
+
+    if operation == "insert_beginning" and data:
+        linked_list.insert_at_beginning(data)
+    elif operation == "insert_end" and data:
+        linked_list.insert_at_end(data)
+    elif operation == "remove_beginning":
+        linked_list.remove_beginning()
+    elif operation == "remove_end":
+        linked_list.remove_end()
+    elif operation == "remove_at" and data:
+        linked_list.remove_at(data)
+
+    return redirect(url_for('show_linked_list'))  
+
 if __name__ == "__main__":
     app.run(debug=True)
-
-
